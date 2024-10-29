763693a220b1c5ae90ce5fb8e5e9af69cd231000
YaliZhang1 <yali.zhang@hyperisland.se>
2024-10-29 13:59:53 +0100

    change the maxAssetSize bigger

--- src/menuPage.js
+++ src/menuPage.js
@@ -139,3 +139,153 @@ function createMenuPage() {
 }
 
 export default createMenuPage;
+
+//lazy loadModule
+// import "./styles.css";
+
+// function createMenuPage() {
+//   // Get the content from the template.html file
+//   const menuPageContent = document.getElementById("content");
+
+//   // Create menu container
+//   const menuContainer = document.createElement("div");
+//   menuContainer.className = "menu-container";
+
+//   // Create menu title
+//   const menuTitle = document.createElement("h1");
+//   menuTitle.textContent = "Our Menu";
+//   menuContainer.appendChild(menuTitle);
+
+//   // Create menu-items-box container
+//   const menuItemsBoxContainer = document.createElement("div");
+//   menuItemsBoxContainer.className = "menu-items-box-container";
+//   menuContainer.appendChild(menuItemsBoxContainer);
+
+//   // Menu categories and their contents
+//   const menuSections = {
+//     Appetizers: {
+//       image: () => import("./assets/appetiser.jpg"), // 使用函数懒加载
+//       items: [
+//         {
+//           name: "Bruschetta",
+//           description: "Grilled bread with tomatoes and basil",
+//           price: "60 kr",
+//         },
+//         {
+//           name: "Stuffed Mushrooms",
+//           description: "Mushrooms with garlic and cheese",
+//           price: "70 kr",
+//         },
+//       ],
+//     },
+//     Entrees: {
+//       image: () => import("./assets/entrees.jpg"), // 使用函数懒加载
+//       items: [
+//         {
+//           name: "Grilled Steak",
+//           description: "Served with seasonal vegetables",
+//           price: "200 kr",
+//         },
+//         {
+//           name: "Linguine Alfredo",
+//           description: "Creamy pasta with parmesan cheese",
+//           price: "120 kr",
+//         },
+//       ],
+//     },
+//     Desserts: {
+//       image: () => import("./assets/desserts.jpg"), // 使用函数懒加载
+//       items: [
+//         {
+//           name: "Chocolate Lava Cake",
+//           description: "Warm cake with a molten center",
+//           price: "80 kr",
+//         },
+//         {
+//           name: "Tiramisu",
+//           description: "Classic Italian dessert with coffee flavor",
+//           price: "90 kr",
+//         },
+//       ],
+//     },
+//     Drinks: {
+//       image: () => import("./assets/drinks.jpg"), // 使用函数懒加载
+//       items: [
+//         {
+//           name: "Coffee",
+//           description: "Freshly brewed coffee",
+//           price: "30 kr",
+//         },
+//         {
+//           name: "House Wine",
+//           description: "Red or white, served by the glass",
+//           price: "70 kr",
+//         },
+//       ],
+//     },
+//   };
+
+//   // Iterate through the menu categories, creating each category and item content
+//   const loadImagesPromises = []; // 存储图片加载的 promises
+//   for (const [section, { image, items }] of Object.entries(menuSections)) {
+//     // Create a container for each section
+//     const sectionContainer = document.createElement("div");
+//     sectionContainer.className = "section-container"; // Class for outer div
+
+//     // Create category titles
+//     const sectionTitle = document.createElement("h2");
+//     sectionTitle.textContent = section;
+//     sectionContainer.appendChild(sectionTitle);
+
+//     // Create category images
+//     const sectionImage = document.createElement("img");
+//     sectionImage.className = "menu-image";
+//     sectionImage.alt = `${section} image`;
+
+//     // Add the image promise to the array
+//     loadImagesPromises.push(
+//       image().then((imageModule) => {
+//         sectionImage.src = imageModule.default; // 设置图片源
+//       })
+//     );
+
+//     sectionContainer.appendChild(sectionImage);
+
+//     // Create a project list
+//     const itemList = document.createElement("div");
+//     itemList.className = "item-list";
+
+//     items.forEach((item) => {
+//       const itemContainer = document.createElement("div");
+//       itemContainer.className = "menu-item";
+
+//       const itemName = document.createElement("h3");
+//       itemName.textContent = item.name;
+//       itemContainer.appendChild(itemName);
+
+//       const itemDescription = document.createElement("p");
+//       itemDescription.textContent = item.description;
+//       itemContainer.appendChild(itemDescription);
+
+//       const itemPrice = document.createElement("p");
+//       itemPrice.className = "price";
+//       itemPrice.textContent = item.price;
+//       itemContainer.appendChild(itemPrice);
+
+//       itemList.appendChild(itemContainer);
+//     });
+
+//     // Add itemList to sectionContainer
+//     sectionContainer.appendChild(itemList);
+
+//     // Add sectionContainer to menuContainer
+//     menuItemsBoxContainer.appendChild(sectionContainer);
+//   }
+
+//   // Wait for all images to load before appending menuContainer to page
+//   Promise.all(loadImagesPromises).then(() => {
+//     menuPageContent.appendChild(menuContainer);
+//   });
+// }
+
+// export default createMenuPage;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000, // Set minimum split size
+      maxSize: 244 * 1024, // Set maximum chunk size to 244 KiB
+      name: false, // Avoid name conflicts
+    },
+  },
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
+  devtool: "source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
   },
@@ -17,6 +26,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
   module: {
     rules: [
@@ -30,8 +40,29 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|gif|jpeg)$/i,
-        type: "asset/resource",
+        // type: "asset/resource",
+        type: "asset",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+        use: [
+          {
+            loader: "image-webpack-loader", //Add image-webpack-loader deal with image files.
+            options: {
+              mozjpeg: { progressive: true, quality: 65 }, //When set to true, this enables progressive loading for JPEG images. Progressive images load in stages, gradually becoming clearer, rather than loading line by line from top to bottom.The value 65 specifies the compression quality for JPEG images, usually on a scale from 0 to 100. Lower values lead to smaller file sizes but reduced image quality.
+              optipng: { enabled: false }, //Setting this to false disables OptiPNG optimization. OptiPNG is a tool for lossless compression of PNG images, which can reduce file size without significantly affecting image quality.
+              pngquant: { quality: [0.65, 0.8], speed: 4 }, //The array [0.65, 0.8] sets the quality range for PNG image compression, where 0.65 is the minimum quality, and 0.8 is the maximum quality. The range allows dynamic compression, balancing reduced file size with good visual quality based on image content.
+              gifsicle: { interlaced: false }, //Setting this to false means that GIF images won’t use interlaced loading. Non-interlaced GIFs load sequentially from top to bottom, whereas interlaced ones load gradually.
+              webp: { quality: 75 }, //The value 75 represents the compression quality for WebP images. WebP is a modern image format that supports high-quality compression while maintaining a smaller file size. Quality values range from 0 to 100, with higher values meaning better quality and larger file size.
+            },
+          },
+        ],
       },
     ],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
